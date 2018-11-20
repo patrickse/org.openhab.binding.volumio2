6fcfc6fb8f1a5ce91c00d0aedd10821bf4bda19d
Patrick Sernetz <patrick@sernetz.com>
2018-11-20 15:39:15 +0100

    Fixed Style Checker Problems

--- src/main/java/org/openhab/binding/volumio2/handler/Volumio2Handler.java
+++ src/main/java/org/openhab/binding/volumio2/handler/Volumio2Handler.java
@@ -1,10 +1,12 @@
 /**
- * Copyright (c) 2014-2015 openHAB UG (haftungsbeschraenkt) and others.
+ * Copyright (c) 2010-2018 by the respective copyright holders.
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
+
 package org.openhab.binding.volumio2.handler;
 
 import static org.openhab.binding.volumio2.Volumio2BindingConstants.*;
@@ -43,7 +45,7 @@ import io.socket.emitter.Emitter;
  * The {@link Volumio2Handler} is responsible for handling commands, which are
  * sent to one of the channels.
  *
- * @author Patrick Sernetz - Initial contribution
+ * @author Patrick Sernetz - Initial Contribution
  */
 public class Volumio2Handler extends BaseThingHandler {
 
--- src/main/java/org/openhab/binding/volumio2/internal/mapping/Volumio2Commands.java
+++ src/main/java/org/openhab/binding/volumio2/internal/mapping/Volumio2Commands.java
@@ -1,10 +1,19 @@
+/**
+ * Copyright (c) 2010-2018 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
 package org.openhab.binding.volumio2.internal.mapping;
 
 /**
  * @see https://github.com/volumio/Volumio2-UI/blob/master/src/app/services/player.service.js
  * @see https://github.com/volumio/Volumio2/blob/master/app/plugins/user_interface/websocket/index.js
  *
- * @author Patrick Sernetz <patrick@sernetz.com>
+ * @author Patrick Sernetz - Initial Contribution
  *
  */
 public class Volumio2Commands {
